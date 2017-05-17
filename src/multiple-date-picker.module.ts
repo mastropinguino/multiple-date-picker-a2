4ad6a58de2b3207b28343cb788a4ee20d45517c2
Vincenzo Farruggia <mastropinguino@networky.net>
2017-05-17 18:44:51 +0200

    Change import BrowserModule to CommonModule to prevent double import error when importing in other modules.

--- src/multiple-date-picker.module.ts
+++ src/multiple-date-picker.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MultipleDatePickerComponent } from './multiple-date-picker.component'
 
 @NgModule({
     imports: [
-        BrowserModule,
+        CommonModule,
         FormsModule
     ],
     declarations: [
